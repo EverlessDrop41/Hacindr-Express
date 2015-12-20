8aaca92fcff2ec132a8edc3131dd92f9d29ff563
emilyperegrine <tomfperegrine@googlemail.com>
2015-12-20 03:14:48 +0000

    Added idea page

--- routes/home.js
+++ routes/home.js
@@ -19,4 +19,17 @@ module.exports = function (app) {
 		  //res.send(response.body);
 		});
   });
+
+  app.get('/queue', app.locals.isLoggedIn, function(req, res) {
+  	unirest.get(app.locals.base_url + "/ideas/next").header({
+			Authorization: req.session.user.authHeader
+		}).end(function (response) {
+			res.render('idea.nunjucks', {user: req.session.user, idea: response.body.idea});
+		  //res.send(response.body);
+		});
+  });
+
+  app.post('/vote/:id/:value', app.locals.isLoggedIn, function(req, res) {
+  	res.send("Voting on the idea with an id of " + req.params.id + " and a value of " + req.params.value);
+  });
 }
